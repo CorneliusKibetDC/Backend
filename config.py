09868e0041eebc3aa60af61633b6d8f0f55d01ac
CorneliusKibetDC <kapslogo18@gmail.com>
2025-02-03 21:28:18 +0300

    gunicorn

--- config.py
+++ config.py
@@ -22,15 +22,39 @@
 
 
 
-#new
+# #new
+# from decouple import config
+# import os
+
+# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+# class Config:
+#     SECRET_KEY = config('SECRET_KEY')
+#     SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
+
+# class DevConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'dev.db')
+#     DEBUG = True
+#     SQLALCHEMY_ECHO = True  # Fixed typo
+
+# class ProdConfig(Config):
+#     # Use the DATABASE_URL provided by Render for PostgreSQL
+#     SQLALCHEMY_DATABASE_URI = config('postgresql://backend_o39f_user:b8YD1GFSbSe3GNi53qnJOcLI52g9ukDB@dpg-cudn230gph6c73ff3o8g-a.oregon-postgres.render.com/dev')  # Render will provide this
+#     DEBUG = False
+#     SQLALCHEMY_ECHO = False
+
+# class TestConfig(Config):
+#     pass
+
+#new 2
 from decouple import config
 import os
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class Config:
-    SECRET_KEY = config('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
+    SECRET_KEY = config('SECRET_KEY', default="mysecretkey")
+    SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool, default=False)
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'dev.db')
@@ -38,11 +62,10 @@ class DevConfig(Config):
     SQLALCHEMY_ECHO = True  # Fixed typo
 
 class ProdConfig(Config):
-    # Use the DATABASE_URL provided by Render for PostgreSQL
-    SQLALCHEMY_DATABASE_URI = config('postgresql://backend_o39f_user:b8YD1GFSbSe3GNi53qnJOcLI52g9ukDB@dpg-cudn230gph6c73ff3o8g-a.oregon-postgres.render.com/dev')  # Render will provide this
+    # Use the DATABASE_URL environment variable from Render
+    SQLALCHEMY_DATABASE_URI = config('postgresql://backend_o39f_user:b8YD1GFSbSe3GNi53qnJOcLI52g9ukDB@dpg-cudn230gph6c73ff3o8g-a.oregon-postgres.render.com/dev', default="")
     DEBUG = False
     SQLALCHEMY_ECHO = False
 
 class TestConfig(Config):
     pass
-
